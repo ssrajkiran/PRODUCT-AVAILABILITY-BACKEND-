c77db7103307ef2a64057e897c8df65a0d569403
ssrajkiran <ssrajkiran1@gmail.com>
2023-04-19 20:07:22 +0530

    Updated Schema

--- Mobile app/productavailability/src/main/java/com/ssrajkiran1/productavailability/exception/ShopNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ssrajkiran1.productavailability.exception;
-
-public class ShopNotFoundException extends RuntimeException {
-    public ShopNotFoundException(String message) {
-        super(message);
-    }
-}
